edcf85582d80548af1b01e74a589aa5616065ea4
lu4nsds <luan.s9d7s@gmail.com>
2021-01-18 12:10:35 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HELLO SEUS FILHA DA PUTA!</Text>
+      <Text style={styles.text}>HELLO !</Text>
       <StatusBar style="auto" />
     </View>
   );
