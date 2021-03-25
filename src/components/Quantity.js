4c34fec7fbe9cd0e8fb2fa6e4880a6574fe0b083
lu4nsds <luan.s9d7s@gmail.com>
2021-03-25 18:02:01 -0300

    almost finishing createad page

--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -5,53 +5,93 @@ import {
     TextInput,
     Modal,
     Image,
+    TouchableOpa
 } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { set } from 'react-native-reanimated'
+import { colors } from '../styles/colors'
+import { texts } from '../styles/texts'
 
 export default function Quantity({ticket}){
     const [numberTicket, setNumberTicket] = useState(ticket)
+
+    function isZero(){
+        if(numberTicket === '0'){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    function numberOfTickets(operation){
+        if(operation){
+            setNumberTicket((numberTicket + 1))
+        } else {
+            setNumberTicket((numberTicket - 1))
+        }
+    }
+
+
     return (
         <View style={{
             flexDirection: 'row',
             width: '100%',
-            minHeight: 50,
+            minHeight: 60,
             marginTop: 10,
-            backgroundColor: '#ffffff',
             justifyContent: 'center',
         }}>
-            <View style={{
-                backgroundColor: '#ffffff',
-                width: '20%',
+            <TouchableOpacity style={{
+                backgroundColor: colors.secondary,
                 alignItems: 'center',
-        
-        }}>
-                <Text>
+                justifyContent: 'center',
+                flex: 1,
+                width: 80,
+                borderTopLeftRadius: 20,
+                borderBottomLeftRadius: 20,
+            }}                           
+            onPress={()=> numberOfTickets(false)}
+            //disabled={() =>isZero()}  
+        >
+                <Text style={{
+                    ...texts.subtitleLight,   
+                }}>
                     -
                 </Text>
-            </View>
+            </TouchableOpacity>
 
             <View style={{
-                backgroundColor: '#000000',
-                width: '60%',
+                backgroundColor: '#ffffff',
+                width: '50%',
                 alignItems: 'center',
+                justifyContent: 'center',   
             }}            
             >
                 <Text style={{
                     alignItems: 'center',
-            
+                    ...texts.subtitleSecondaryDark,       
                 }}>
                     {numberTicket}
                 </Text>
             </View>
 
-            <View style= {{
-                backgroundColor: '#C9c9c9',
-                width: '20%',
+            <TouchableOpacity style= {{
+                backgroundColor: colors.secondary,
+                flex: 1,
+                width: 80,
                 alignItems: 'center',
-            }}>
-                <Text>
+                justifyContent: 'center',
+                borderTopRightRadius: 20,
+                borderBottomRightRadius: 20,
+            }}
+            onPress={()=> numberOfTickets(true)}
+            >
+                <Text style={{
+                    ...texts.subtitleLight,   
+                }}>
                     +
                 </Text>
-            </View>
+            </TouchableOpacity>
         </View>
 
         )
