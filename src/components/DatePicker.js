9d2c0eef10e97af5e1f584677fe76e2d0f954fb8
lu4nsds <luan.s9d7s@gmail.com>
2021-03-30 17:57:00 -0300

    building the formatData function

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -6,7 +6,9 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import {
     View,
     Text,
+    Dimensions,
     Button,
+    Image,
     TouchableOpacity,
     Thumbnail,
 } from 'react-native'
@@ -14,7 +16,7 @@ import {
 export function DatePicker(){
     const [date, setDate] = useState(new Date());
     const [show, setShow] = useState(false);
-    const [dateString, setDateString] = useState(`${date.getDay()}/ ${date.getMonth()}/ ${date.getFullYear()}`);
+    const [dateString, setDateString] = useState(formatData(date.getDate(), date.getMonth, date.getFullYear));
 
     const onChange = (event, selectedDate) => {
         if (selectedDate === undefined) {
@@ -30,21 +32,95 @@ export function DatePicker(){
      
     function showDatepicker() {
        setShow(true);
-    }  
+    } 
+    
+    
+    function formatData({day, month, year}){
+        const d = day
+        const m = month
+        const y = year
+        
+        if(day < 10){
+            d = '0'+ `${day}`
+            if(month == 0){
+                m = 'Jan'
+            } else if (month == 1){
+                m = 'Fev'
+            } else if (month == 2){
+                m = 'Mar'
+            } else if (month == 3){
+                m = 'Abr'
+            }else if (month == 4){
+                m = 'Mai'
+            }else if (month == 5){
+                m = 'Jun'
+            }else if (month == 6){
+                m = 'Jul'
+            }else if (month == 7){
+                m = 'Ago'
+            }else if (month == 8){
+                m = 'Set'
+            }else if (month == 9){
+                m = 'Out'
+            }else if (month == 10){
+                m = 'Nov'
+            }else if (month == 11){
+                m = 'Dez'
+            }        
+        } else {
+            if(month == 0){
+                m = 'Jan'
+            } else if (month == 1){
+                m = 'Fev'
+            } else if (month == 2){
+                m = 'Mar'
+            } else if (month == 3){
+                m = 'Abr'
+            }else if (month == 4){
+                m = 'Mai'
+            }else if (month == 5){
+                m = 'Jun'
+            }else if (month == 6){
+                m = 'Jul'
+            }else if (month == 7){
+                m = 'Ago'
+            }else if (month == 8){
+                m = 'Set'
+            }else if (month == 9){
+                m = 'Out'
+            }else if (month == 10){
+                m = 'Nov'
+            }else if (month == 11){
+                m = 'Dez'
+            }
+        }
+    
+        return (`${d}/${m}/${y}`)
+    }
 
     return (
         <View>
             <TouchableOpacity
             style={{
-                width: 150,
-                height: 150,
-                backgroundColor: '#ffffff',
-                justifyContent: 'center',
-                alignItems: 'center',
+                    width: Dimensions.get('window').width * 0.8,
+                    borderRadius: 15,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    minHeight: 60,
+                    maxHeight: 60,
+                    borderWidth: 1,
+                    color: colors.light,
+                    borderColor: colors.light,
+                    ...texts.textBoldLight,
+                    marginVertical: Dimensions.get('window').height * 0.01,
             }}
+                    
             onPress={()=> showDatepicker()}
             >
-            <Text>
+            <Image style={styles.imageInput}
+                source={require("../../assets/Icons/calendar.png")} />
+            <Text style={styles.textLight}>
                 {dateString}
             </Text>
                 
