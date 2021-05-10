916820384a12c584ee3fc0dcbf0afed956ac0c7c
srgesio <gesioiajm@gmail.com>
2021-05-09 22:08:09 -0300

    fixing dependencies

--- src/api.js
+++ src/api.js
@@ -2,13 +2,13 @@ import axios from "axios"
 
 const api = axios.create({
   //LUAN
-  baseURL:"http://192.168.1.64:3333",
+  //baseURL:"http://192.168.1.64:3333",
 
   //ARIEL
   //baseURL: "http://192.168.1.153:3333",
 
   //GESIO
-  //baseURL: "http://192.168.100.102:3333",
+  baseURL: "http://192.168.100.102:3333",
 })
 export const config = "http://192.168.100.102:3333"
 export default api
